6a0459dbc3d5161acac0e5760a52b94a6d8933f9
Jyoti Raj <jyotipanday.jp1725@gmail.com>
2024-08-22 10:58:13 +0530

    Update Temp_conversion_program.py

--- Temp_conversion_program.py
+++ Temp_conversion_program.py
@@ -12,42 +12,49 @@ print("Please select Conversion type: \n"
 
 select_1=int(input("Enter option(1-6) : "))
 
-#Asking user to input temperature
-degree=float(input("Enter temperature: "))
-
-
 # Applying condition for conversion type=1 (celsius to fahrenheit)
 if select_1==1:
+   #Asking user to input temperature
+   degree=float(input("Enter temperature: "))
    fahrenheit = (9/5) * degree + 32
    print(degree,"Celsius = ",fahrenheit,"Fahrenheit")
 
 # Applying condition for conversion type=2 (celsius to kelvin)
 elif select_1==2:
+  #Asking user to input temperature
+  degree=float(input("Enter temperature: "))
   kelvin = degree + 273.25
   print(degree,"Celsius = ",kelvin,"Kelvin")
 
 #Applying condition for conversion type=3 (fahrenheit to celsius)
 elif select_1==3:
+  #Asking user to input temperature
+  degree=float(input("Enter temperature: "))
   celsius = (degree-32) * 5/9
   print(degree,"Fahrenheit = ",celsius,"Celsius")
 
 # Applying condition for conversion type=4 (fahrenheit to kelvin)
 elif select_1==4:
+  #Asking user to input temperature
+  degree=float(input("Enter temperature: "))
   kelvin = (degree-32) * 5/9 + 273.15
   print(degree,"Fahrenheit = ",kelvin,"Kelvin")
 
 # Applying condition for conversion type=5 (kelvin to celsius )
 elif select_1==5:
+ #Asking user to input temperature
+ degree=float(input("Enter temperature: "))
  celsius=degree-273.15
  print(degree,"Kelvin = ",celsius,"Celsius")
 
 # Applying condition for conversion type=6 (kelvin to fahrenheit)
 elif select_1==6:
+ #Asking user to input temperature
+ degree=float(input("Enter temperature: "))
  fahrenheit=(degree-273.15) * 9/5 + 32
  print(degree,"Kelvin = ",fahrenheit,"Fahrenheit")   
    
 else:
-print("invalid input")
+ print("invalid input")
  
-   
-        
+
